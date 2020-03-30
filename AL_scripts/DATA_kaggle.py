9c886982a05b0153ae885186ffb134ea54fea8b9
MatildeDP <mat0503@hotmail.com>
2020-03-30 21:03:22 +0200

    mere på Xray

--- AL_scripts/DATA_kaggle.py
+++ AL_scripts/DATA_kaggle.py
@@ -6,10 +6,16 @@ Created on Thu Mar 26 10:37:11 2020
 """
 
 import numpy as np
-import torch
-from torchvision import datasets
-from torch.utils.data import Dataset
+#from torchvision import datasets
+import zipfile
 from PIL import Image
+from matplotlib import image
+from matplotlib import pyplot
+import glob
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
+
+#%% Dowload from kaggle (needs login)
 
 from kaggle.api.kaggle_api_extended import KaggleApi
 api = KaggleApi()
@@ -19,38 +25,111 @@ api.dataset_download_files('paultimothymooney/chest-xray-pneumonia', path = '.\E
 api.dataset_list_files('paultimothymooney/chest-xray-pneumonia').files
 
 
-import io, pygame, zipfile
-from zipfile import ZipFile 
-zip_path = ".\Egne_filer\chest-xray-pneumonia.zip"
-zf = zipfile.ZipFile(zip_path)
-# read the images of zip via dataloader
-train_loader = torch.utils.data.DataLoader(
-                   DataSet(zf, transform),
-                   batch_size = args.batch_size,
-                   shuffle = True,
-                   num_workers = args.workers,
-                   pin_memory=True)
-  
-# specifying the zip file name 
+#%% unzip into train, test and val
+
 file_name = ".\Egne_filer\chest-xray-pneumonia.zip"
-  
-# opening the zip file in READ mode 
-with ZipFile(file_name, 'r') as zip: 
-    # printing all the contents of the zip file 
-    zip.printdir() 
-  
-    # extracting all the files 
-    print('Extracting all the files now...') 
-    zip.extractall() 
-    print('Done!') 
+
+archive = zipfile.ZipFile(file_name)
+save_path = '.\Egne_filer\Train'
+for file in archive.namelist():
+    if file.startswith('chest_xray/train/'):
+        archive.extract(file, save_path)
+        
+archive = zipfile.ZipFile(file_name)
+save_path = '.\Egne_filer\Val'
+for file in archive.namelist():
+    if file.startswith('chest_xray/val/'):
+        archive.extract(file, save_path)
+        
+archive = zipfile.ZipFile(file_name)
+save_path = '.\Egne_filer\Test'
+for file in archive.namelist():
+    if file.startswith('chest_xray/test/'):
+        archive.extract(file, save_path)
+
+
+
+#%% access images from folders and save in numpy 
+#shape of array
+#PNEUMONIA = 1
+#Normal = 0
+M1, N1 = 1858,2090
+path_train = r'./Egne_filer/Train/chest_xray/train/' 
+path_test = r'./Egne_filer/Test/chest_xray/test/' 
+def get_Xray(path_train, path_test, M1, N1):     
+#path: path containing the folder "NORMAL" or "PNEUMONIA" 
+
+    #TRAINING IMAGES
+    #Normal images    
+    all_files = glob.glob(path_train + "NORMAL" +"/*.jpeg")
+    val0 = np.zeros((len(all_files),M1,N1))
+    y0 = np.asarray([0]*len(all_files))
+    for i, filename in enumerate(all_files):
+        im = Image.open(filename)
+        data = np.asarray(im)
+        M,N = data.shape
+        M2 = int((M1 - M)/2)
+        N2 = int((N1 - N)/2)
+        
+        val0[i,M2:M+M2,N2:N+N2] = data
+        
+    #Pneumonia images
+    all_files = glob.glob(path_train + "PNEUMONIA" +"/*.jpeg")
+    val1 = np.zeros((len(all_files),M1,N1))
+    y1 = np.asarray([1]*len(all_files))
+    for j, filename in enumerate(all_files):
+        im = Image.open(filename)
+        data = np.asarray(im)
+        M,N = data.shape
+        M2 = int((M1 - M)/2)
+        N2 = int((N1 - N)/2)
+        
+        val1[j,M2:M+M2,N2:N+N2] = data
+        
+    #Concatenate   
+    X_tr = np.concatenate((val0,val1),axis=0)   
+    Y_tr = np.concatenate((y0,y1))
     
+    
+    #Normal images    
+    all_files = glob.glob(path_test + "NORMAL" +"/*.jpeg")
+    val0 = np.zeros((len(all_files),M1,N1))
+    y0 = np.asarray([0]*len(all_files))
+    for i, filename in enumerate(all_files):
+        im = Image.open(filename)
+        data = np.asarray(im)
+        M,N = data.shape
+        M2 = int((M1 - M)/2)
+        N2 = int((N1 - N)/2)
+        
+        val0[i,M2:M+M2,N2:N+N2] = data
+        
+    #Pneumonia images
+    all_files = glob.glob(path_test + "PNEUMONIA" +"/*.jpeg")
+    val1 = np.zeros((len(all_files),M1,N1))
+    y1 = np.asarray([1]*len(all_files))
+    for j, filename in enumerate(all_files):
+        im = Image.open(filename)
+        data = np.asarray(im)
+        M,N = data.shape
+        M2 = int((M1 - M)/2)
+        N2 = int((N1 - N)/2)
+        
+        val1[j,M2:M+M2,N2:N+N2] = data
+    #concatenate
+    
+    X_te = np.concatenate((val0,val1),axis=0)   
+    Y_te = np.concatenate((y0,y1))
+    
+    return X_tr, Y_tr, X_te, Y_te
+
+X_tr, Y_tr, X_te, Y_te = get_Xray(path_train, path_test, M1, N1)
 
 
-import zipfile
-from StringIO import StringIO
-from PIL import Image
-import imghdr
 
-imgzip = open('100-Test.zip')
-zippedImgs = zipfile.ZipFile(imgzip)
 
+
+
+#print images
+im2 = Image.fromarray(val[0])
+im2.show()
