1593c9c8dc1c17a6294f3ee94b6ed5df58d490d8
Lukas Leindals <46963040+s183920@users.noreply.github.com>
2020-03-31 19:58:07 +0200

    add qbc and remove covid dataset

--- AL_scripts/run_functions.py
+++ AL_scripts/run_functions.py
@@ -1,18 +1,17 @@
 import numpy as np
 from dataloader import get_dataset, get_handler, get_args
 from model import get_model
-from query_strategies import RandomSampling, UncertaintySampling, MarginSampling
+from query_strategies import RandomSampling, UncertaintySampling, MarginSampling, QBCSampling
 import torch
 import pickle
+import time
 # import random
 
 
-def train_rounds(X_te, Y_te, n_rounds, n_query, n_train, ids_labeled, strategy, dataset, seed):
+def train_rounds(X_te, Y_te, n_rounds, n_query, n_train, ids_labeled, strategy, seed):
     # calculate accuracies for the rounds
     acc = np.zeros(n_rounds+1)
     for rd in range(n_rounds+1):
-        np.random.seed(seed)
-        torch.manual_seed(seed)
         if rd != 0: #in round 0 our model has not been trained and we can therefore not use our query strategies
             #query
             q_ids = strategy.query(n_query)
@@ -20,15 +19,15 @@ def train_rounds(X_te, Y_te, n_rounds, n_query, n_train, ids_labeled, strategy, 
 
             #update which labels are id'ed
             strategy.update(ids_labeled)
-
+        
+        np.random.seed(seed)
+        torch.manual_seed(seed)
         strategy.train()
         preds = strategy.predict(X_te, Y_te)
         acc[rd] = 1.0*(Y_te.to(dtype=torch.int64) == preds).sum().item() / len(Y_te)
         print(f"The testing accuracy for round {rd} with {n_train+rd*n_query} training samples was {round(acc[rd], 2)*100} %") #/{sum(ids_labeled)}
 
-    # save results
-    with open("AL_scripts/results/"+dataset+ "_" +strategy.get_name()+ "_accuracies_test" +".pkl", "wb") as file:
-        pickle.dump(acc, file)
+    return acc
 
 
 def run_AL(n_train, n_query, n_rounds, dataset, strat, seed = 1):
@@ -47,6 +46,8 @@ def run_AL(n_train, n_query, n_rounds, dataset, strat, seed = 1):
 
     # load dataset
     X_tr, Y_tr, X_te, Y_te = get_dataset(dataset)
+    # X_tr = X_tr[:10000]
+    # Y_tr = Y_tr[:10000]
 
 
     # inialize experiment
@@ -70,6 +71,8 @@ def run_AL(n_train, n_query, n_rounds, dataset, strat, seed = 1):
         strategy = UncertaintySampling(X_tr, Y_tr, ids_labeled, model, handler, args)
     elif strat.lower() == "margin":
         strategy = MarginSampling(X_tr, Y_tr, ids_labeled, model, handler, args)
+    elif strat.lower() == "qbc":
+        strategy = QBCSampling(X_tr, Y_tr, ids_labeled, model, handler, args)
 
     # print info
     print("-"*20+" INFO "+"-"*20)
@@ -81,6 +84,10 @@ def run_AL(n_train, n_query, n_rounds, dataset, strat, seed = 1):
     print("The strategy is ", strategy.get_name())
     print("-"*46)
 
-    train_rounds(X_te, Y_te, n_rounds, n_query, n_train, ids_labeled, strategy, dataset, seed)
-
-
+    t0 = time.time()
+    acc = train_rounds(X_te, Y_te, n_rounds, n_query, n_train, ids_labeled, strategy, seed)
+    print("Done!")
+    print(f"Time for training: {time.time()-t0}")
+    # save results
+    with open("AL_scripts/results/"+dataset+ "_" +strategy.get_name()+ "_accuracies" +".pkl", "wb") as file:
+        pickle.dump(acc, file)
